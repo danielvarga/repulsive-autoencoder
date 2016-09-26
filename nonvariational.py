c3072ba1806cb2dcc147b529e73411fde1ae4c25
Daniel Varga <daniel.varga@prezi.com>
2016-09-26 15:49:42 +0200

    for reference, the 0-variance version of VAE

---
+++ nonvariational.py
@@ -0,0 +1,99 @@
+'''This script demonstrates how to build a variational autoencoder with Keras.
+
+Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114
+'''
+import numpy as np
+
+np.random.seed(1337)
+
+import matplotlib.pyplot as plt
+
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model
+from keras import backend as K
+from keras import objectives
+from keras.datasets import mnist
+
+
+batch_size = 500
+original_dim = 784
+latent_dim = 2
+intermediate_dim = 256
+nb_epoch = 50
+
+x = Input(batch_shape=(batch_size, original_dim))
+h = Dense(intermediate_dim, activation='relu')(x)
+
+# Completely got rid of the variational aspect
+z = Dense(latent_dim)(h)
+
+
+# we instantiate these layers separately so as to reuse them later
+decoder_h = Dense(intermediate_dim, activation='relu')
+decoder = Dense(original_dim, activation='sigmoid')
+h_decoded = decoder_h(z)
+x_decoded = decoder(h_decoded)
+
+
+def vae_loss(x, x_decoded):
+    # DANIEL Why not mean_squared_error?
+    xent_loss = original_dim * objectives.binary_crossentropy(x, x_decoded)
+    # DANIEL I've destroyed the variance vars.
+    # http://mlg.postech.ac.kr/~seungjin/courses/ml/handouts/note02.pdf
+    # Maybe some better normality test would be nicer here? This is just L2 reg on the latent vars, that's lame.
+    # Kolmogorov-Smirnov requires projection and sorting, that's probably an overkill. What else?
+    kl_loss = - 0.5 * K.sum(K.square(z), axis=-1)
+    return xent_loss + kl_loss / 3000 # Don't know the right choice for this constant.
+
+vae = Model(x, x_decoded)
+vae.compile(optimizer='rmsprop', loss=vae_loss)
+
+# train the VAE on MNIST digits
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+vae.fit(x_train, x_train,
+        shuffle=True,
+        nb_epoch=nb_epoch,
+        batch_size=batch_size,
+        validation_data=(x_test, x_test))
+
+# build a model to project inputs on the latent space
+encoder = Model(x, z)
+
+# display a 2D plot of the digit classes in the latent space
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+plt.figure(figsize=(6, 6))
+plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
+plt.colorbar()
+plt.savefig("fig1.png")
+
+# build a digit generator that can sample from the learned distribution
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(decoder_input)
+_x_decoded = decoder(_h_decoded)
+generator = Model(decoder_input, _x_decoded)
+
+# display a 2D manifold of the digits
+n = 15  # figure with 15x15 digits
+digit_size = 28
+figure = np.zeros((digit_size * n, digit_size * n))
+# we will sample n points within [-15, 15] standard deviations
+grid_x = np.linspace(-15, 15, n)
+grid_y = np.linspace(-15, 15, n)
+
+for i, yi in enumerate(grid_x):
+    for j, xi in enumerate(grid_y):
+        z_sample = np.array([[xi, yi]])
+        x_decoded = generator.predict(z_sample)
+        digit = x_decoded[0].reshape(digit_size, digit_size)
+        figure[i * digit_size: (i + 1) * digit_size,
+               j * digit_size: (j + 1) * digit_size] = digit
+
+plt.figure(figsize=(10, 10))
+plt.imshow(figure)
+plt.savefig("fig2.png")

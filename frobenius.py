69e97e107ebf6778ff85f4a9f7f2e5802b4fab7f
Daniel Varga <daniel.varga@prezi.com>
2016-10-15 01:38:39 +0200

    experiments with squared and non-squared pairwise distances.

---
+++ frobenius.py
@@ -0,0 +1,82 @@
+'''
+This is experiment code for a little spinoff project of the repulsive autoencoder.
+
+I needed an energy function with the property that in the limit, the optimal point set
+has uniform density on the unit sphere. Something like the Coulomb force and the Thomson problem,
+but I wanted something numerically more stable. I came up with the following:
+
+The energy term for the interaction of two particles is their squared scalar product.
+(Equivalently, their negative squared L2 distance, but let's stick to squared scalar product.)
+The total energy of the system is the sum over all pairs of particles.
+
+Numerically solving the optimization problem, I've observed that with n points in m dimensions,
+the optimal total energy was always exactly n^2/m. (For n>=m)
+
+Adrian Csiszarik has managed to prove this with a simple but very attractive spectral argument.
+
+Even without Adrian's characterization, it is easy to see in retrospect that my
+proposed energy function is absolutely awful for its intended purpose of enforcing
+a uniform arrangement. In contrast, if we use non-squared, standard distance
+instead of squared distance, that energy function does have this smoothing property.
+'''
+
+import math
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+np.random.seed(1337)
+
+
+n = 9 # particle count
+m = 3 # dimension of space
+epochs = 100 # iteration count
+
+
+def main():
+    x = tf.Variable(np.random.normal(size=(n, m)))
+    x_norm = tf.nn.l2_normalize(x, dim=1) # rows normalized
+    rowwise_scalar_product = tf.matmul(x_norm, tf.transpose(x_norm))
+    frobenius = tf.reduce_sum(tf.square(rowwise_scalar_product))
+
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+    train_step = optimizer.minimize(frobenius)
+
+    with tf.Session() as sess:
+        init = tf.initialize_all_variables()
+        sess.run(init)
+        for step in xrange(epochs):
+            sess.run(train_step)
+            print frobenius.eval(session=sess)
+
+        x_val = x_norm.eval(session=sess)
+        print "============== X"
+        print x_val
+        rsc = rowwise_scalar_product.eval(session=sess)
+
+        print "============== XX^T"
+        print rsc
+        print "============== eigenvalues of XX^T"
+        w, v = np.linalg.eig(rsc)
+        print sorted(w.real.tolist(), reverse=True)
+        print "============== Frobenius(XX^T)"
+        print frobenius.eval(session=sess)
+
+    interactive = True
+    if interactive:
+        from mpl_toolkits.mplot3d import Axes3D
+        fig = plt.figure()
+        if m==3:
+            xs, ys, zs = x_val.T
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(xs, ys, zs)
+        elif m==2:
+            xs, ys = x_val.T
+            ax = fig.add_subplot(111)
+            ax.scatter(xs, ys)
+        else:
+            assert m in (2, 3)
+        plt.show()
+
+
+main()
--- springs_tf.py
+++ springs_tf.py
@@ -10,40 +10,76 @@ import matplotlib.pyplot as plt
 np.random.seed(1337)
 
 
-n = 100 # particle count
+n = 12 # particle count
 m = 3 # dimension of space
-epochs = 500 # iteration count
+epochs = 100 # iteration count
+
+
+def squaredDistanceTest():
+    l_a = 8
+    l_b = 9
+    dim = 10
+    a = tf.Variable(np.random.normal(size=(l_a, dim)))
+    b = tf.Variable(np.random.normal(size=(l_b, dim)))
+    aL2S = tf.reduce_sum(a**2, -1, keep_dims=True)
+    bL2S = tf.reduce_sum(b**2, -1, keep_dims=True)
+    aL2SM = tf.reshape(tf.tile(aL2S, (l_b, 1)), (l_b, l_a))
+    bL2SM = tf.reshape(tf.tile(bL2S, (l_a, 1)), (l_a, l_b))
+    print aL2SM.get_shape().as_list()
+    print bL2SM.get_shape().as_list()
+    squaredDistances = aL2SM + tf.transpose(bL2SM) - 2.0 * tf.matmul(b, tf.transpose(a))
+    with tf.Session() as sess:
+        init = tf.initialize_all_variables()
+        sess.run(init)
+        a_val = a.eval(session=sess)
+        b_val = b.eval(session=sess)
+        sd = squaredDistances.eval(session=sess)
+        print sd
+        print "----"
+        print np.array([[(al-bl).dot(al-bl) for al in a_val] for bl in b_val])
 
 
 def main():
     x = tf.Variable(np.random.normal(size=(n, m)))
     x_norm = tf.nn.l2_normalize(x, dim=1) # rows normalized
-    rowwise_scalar_product = tf.matmul(x_norm, tf.transpose(x_norm))
-    frobenius = tf.reduce_sum(tf.square(rowwise_scalar_product))
+
+    epsilon = 0.0001
+    distances = tf.sqrt(2.0 + epsilon - 2.0 * tf.matmul(x_norm, tf.transpose(x_norm)))
+
+    energy = -tf.reduce_sum(distances)
 
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-    train_step = optimizer.minimize(frobenius)
+    train_step = optimizer.minimize(energy)
 
     with tf.Session() as sess:
         init = tf.initialize_all_variables()
         sess.run(init)
         for step in xrange(epochs):
             sess.run(train_step)
-            print frobenius.eval(session=sess)
+            print energy.eval(session=sess)
 
         x_val = x_norm.eval(session=sess)
-        # print x_val
-        print rowwise_scalar_product.eval(session=sess)
-        print frobenius.eval(session=sess)
+        print "============== X"
+        print x_val
+        distances_val = distances.eval(session=sess)
+
+        print "============== Pairwise distances"
+        print distances_val
 
     interactive = True
     if interactive:
         from mpl_toolkits.mplot3d import Axes3D
         fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        assert m==3
-        xs, ys, zs = x_val.T
-        ax.scatter(xs, ys, zs)
+        if m==3:
+            xs, ys, zs = x_val.T
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(xs, ys, zs)
+        elif m==2:
+            xs, ys = x_val.T
+            ax = fig.add_subplot(111)
+            ax.scatter(xs, ys)
+        else:
+            assert m in (2, 3)
         plt.show()
 
 

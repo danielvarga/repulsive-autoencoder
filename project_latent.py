e68d647422cd85436a6ae626704282d51a3f7b21
Daniel Varga <daniel.varga@prezi.com>
2016-11-17 19:40:52 +0100

    cov eigenvals, masking.

--- project_latent.py
+++ project_latent.py
@@ -12,15 +12,43 @@ import model
 
 # encoder = vis.loadModel("/home/zombori/repulsive-autoencoder/pictures/vae_baseline_300_encoder")
 # generator = vis.loadModel("/home/zombori/repulsive-autoencoder/pictures/vae_baseline_300_generator")
+# batch_size = 1000
+
 encoder = vis.loadModel("/home/csadrian/repulsive-autoencoder/models/disc_3_1000_d2_vae/disc_3_1000_d2_vae_encoder")
 generator = vis.loadModel("/home/csadrian/repulsive-autoencoder/models/disc_3_1000_d2_vae/disc_3_1000_d2_vae_generator")
+batch_size = 250
+
+# encoder = vis.loadModel("/home/csadrian/repulsive-autoencoder/models/disc_3_1000_d3_vae/disc_3_1000_d3_vae_encoder")
+# generator = vis.loadModel("/home/csadrian/repulsive-autoencoder/models/disc_3_1000_d3_vae/disc_3_1000_d3_vae_generator")
 
 
 (x_train, x_test), (height, width) = data.load("celeba")
-latent_train = encoder.predict(x_train, batch_size = 250)
-latent_test = encoder.predict(x_test, batch_size = 250)
+latent_train = encoder.predict(x_train, batch_size = batch_size)
+latent_test = encoder.predict(x_test, batch_size = batch_size)
+
+print "latent_train.shape", latent_train.shape
+
 np.savez("latent_train.npz", latent_train)
 np.savez("latent_test.npz", latent_test)
+
+variances = np.var(latent_train, axis=0)
+working_mask = (variances > 0.2)
+print np.sum(working_mask), "/", working_mask.shape
+print np.histogram(variances)
+
+n = latent_train.shape[1]
+
+def masked_sampler(batch_size, latent_dim):
+    z = np.random.normal(size=(batch_size, latent_dim))
+    return z * working_mask
+
+
+vis.displayRandom(n=10, latent_dim=n, sampler=masked_sampler,
+        generator=generator, height=72, width=60, name="masked.png", batch_size=batch_size)
+
+
+
+
 
 projector = GaussianRandomProjection(n_components=2, random_state=81)
 projected_train = projector.fit_transform(latent_train)
@@ -45,6 +73,8 @@ plt.savefig(prefix + "_hexbin.png")
 corr_train = np.corrcoef(latent_train.T)
 corr_test = np.corrcoef(latent_test.T)
 
+
+
 plt.figure(figsize=(12,24))
 ax1 = plt.subplot(211)
 ax1.matshow(np.abs(corr_train), cmap='coolwarm')
@@ -53,12 +83,15 @@ ax2.matshow(np.abs(corr_test), cmap='coolwarm')
 plt.savefig(prefix + "_cov.png")
 
 cov_train = np.cov(latent_train.T)
+
+print "cov_train eigvals = ", sorted(np.linalg.eigvals(cov_train).tolist(), reverse=True)
+
+
 print "CS", cov_train.shape
 mean_train = np.mean(latent_train, axis=0)
 print "MS", mean_train.shape
 cho = np.linalg.cholesky(cov_train)
 print "CHOS", cho.shape
-n = cho.shape[0]
 N = 100000
 z = np.random.normal(0.0, 1.0, (N, n))
 sample = cho.dot(z.T).T+mean_train
@@ -74,14 +107,14 @@ plt.savefig(prefix + "_cov_learned.png")
 
 
 vis.displayRandom(n=50, latent_dim=n, sampler=model.gaussian_sampler,
-        generator=generator, height=72, width=60, name="standard.png", batch_size=250)
+        generator=generator, height=72, width=60, name="standard.png", batch_size=batch_size)
 
 def oval_sampler(batch_size, latent_dim):
     z = np.random.normal(size=(batch_size, latent_dim))
     return cho.dot(z.T).T+mean_train
 
 vis.displayRandom(n=50, latent_dim=n, sampler=oval_sampler,
-        generator=generator, height=72, width=60, name="oval.png", batch_size=250)
+        generator=generator, height=72, width=60, name="oval.png", batch_size=batch_size)
 
 
 do_tsne = True

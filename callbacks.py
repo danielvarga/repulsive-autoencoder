2d9b550414eb996232ae7216ca32e53391879833
Zsolt Zombori <zsolt.zombori@t-online.hu>
2016-12-02 13:40:05 +0100

    added callback for changing loss component weights

--- callbacks.py
+++ callbacks.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from keras.callbacks import LearningRateScheduler, Callback
+from keras import backend as K
 import numpy as np
 import vis
 
@@ -21,7 +22,7 @@ class imageDisplayCallback(Callback):
     def __init__(self, 
                  x_train, x_test, 
                  latent_dim, batch_size,
-                 encoder, generator, sampler, 
+                 encoder, encoder_var, generator, sampler, 
                  name,
                  frequency,
                  **kwargs):
@@ -30,6 +31,7 @@ class imageDisplayCallback(Callback):
         self.latent_dim = latent_dim
         self.batch_size = batch_size
         self.encoder = encoder
+        self.encoder_var = encoder_var
         self.generator = generator
         self.sampler = sampler
         self.name = name
@@ -44,22 +46,31 @@ class imageDisplayCallback(Callback):
         vis.displaySet(self.x_test[:self.batch_size], 100, self.model, "%s-test-%i" % (self.name,epoch+1))
         vis.displaySet(self.x_train[:self.batch_size], 100, self.model, "%s-train-%i" % (self.name,epoch+1))
         vis.displayInterp(self.x_train, self.x_test, self.batch_size, self.latent_dim, self.encoder, self.generator, 10, "%s-interp-%i" % (self.name,epoch+1))
+        if self.encoder != self.encoder_var:
+            vis.plotMVVM(self.x_train, self.encoder, self.encoder_var, self.batch_size, "{}-mvvm-{}.png".format(self.name, epoch+1))
+        vis.plotMVhist(self.x_train, self.encoder, self.batch_size, "{}-mvhist-{}.png".format(self.name, epoch+1))
 
-class meanVarPlotCallback(Callback):
-    def __init__(self, x_train, batch_size, encoder, encoder_var, name, **kwargs):
-        self.x_train = x_train
-        self.batch_size = batch_size
-        self.encoder = encoder
-        self.encoder_var = encoder_var
-        self.name = name
-        super(meanVarPlotCallback, self).__init__(**kwargs)
+
+class weightSchedulerCallback(Callback):
+    # weightPrimary and weightSecondary should be Keras variables
+    def __init__(self, nb_epoch, weightPrimary, weightSecondary, start, stop, **kwargs):
+        self.nb_epoch = nb_epoch
+        self.weightPrimary = weightPrimary
+        self.weightSecondary = weightSecondary
+        self.start = start
+        self.stop = stop
+        super(weightSchedulerCallback, self).__init__(**kwargs)
+
     def on_epoch_end(self, epoch, logs):
-        latent_train_mean = self.encoder.predict(self.x_train, batch_size = self.batch_size)
-        latent_train_logvar = self.encoder_var.predict(self.x_train, batch_size = self.batch_size)
-        mean_variances = np.var(latent_train_mean, axis=0)
-        variance_means = np.mean(np.exp(latent_train_logvar), axis=0)
-        plt.scatter(mean_variances, variance_means)
-        plt.xlim(-1, 12)
-        plt.ylim(-1, 3)
-        plt.savefig("{}_mvvm_{}.png".format(self.name,epoch+1))
+        phase = 1.0 * (epoch+1) / self.nb_epoch
+        if phase < self.start:
+            K.set_value(self.weightPrimary, 1)
+            K.set_value(self.weightSecondary, 0)
+        elif phase < self.stop:
+            K.set_value(self.weightSecondary, (phase-self.start) / (self.stop - self.start))
+            K.set_value(self.weightPrimary, (self.stop - phase) / (self.stop - self.start))
+        else:
+            K.set_value(self.weightPrimary, 0)
+            K.set_value(self.weightSecondary, 1)
+        print "\n{}: Primary - secondary weights: ({} - {})".format(phase, K.eval(self.weightPrimary), K.eval(self.weightSecondary))
         

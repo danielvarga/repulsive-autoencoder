ae4d88b041fc88fff5b5ae39cd31b4071401f8ec
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-01-06 14:09:49 +0100

    added weight decay to the encoder/decoder networks

--- dense.py
+++ dense.py
@@ -1,5 +1,6 @@
 import numpy as np
 from keras.layers import Dense, Flatten, Activation, Reshape, Input
+from keras.regularizers import l2
 
 class Encoder(object):
     pass
@@ -8,31 +9,33 @@ class Decoder(object):
     pass
 
 class DenseEncoder(Encoder):
-    def __init__(self, intermediate_dims, activation):
+    def __init__(self, intermediate_dims, activation, wd):
         self.intermediate_dims = intermediate_dims
         self.activation = activation
+        self.wd = wd
 
     def __call__(self, x):
         h = Flatten()(x)
         for intermediate_dim in self.intermediate_dims:
-            h = Dense(intermediate_dim)(h)
+            h = Dense(intermediate_dim, W_regularizer=l2(self.wd))(h)
             h = Activation(self.activation)(h)
         return h
 
 class DenseDecoder(Decoder):
-    def __init__(self, latent_dim, intermediate_dims, original_shape, activation):
+    def __init__(self, latent_dim, intermediate_dims, original_shape, activation, wd):
         self.latent_dim = latent_dim
         self.intermediate_dims = intermediate_dims
         self.original_shape = original_shape
         self.activation = activation
+        self.wd = wd
 
     def __call__(self, recons_input):
         # we instantiate these layers separately so as to reuse them both for reconstruction and generation
         layers = []
         for intermediate_dim in reversed(self.intermediate_dims):
-            layers.append(Dense(intermediate_dim))
+            layers.append(Dense(intermediate_dim, W_regularizer=l2(self.wd)))
             layers.append(Activation(self.activation))
-        layers.append(Dense(np.prod(self.original_shape), activation='sigmoid', name="decoder_top"))
+        layers.append(Dense(np.prod(self.original_shape), activation='sigmoid', name="decoder_top", W_regularizer=l2(self.wd)))
         layers.append(Reshape(self.original_shape))
 
         generator_input = Input(shape=(self.latent_dim,))

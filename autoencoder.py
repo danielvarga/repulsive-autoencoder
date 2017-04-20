c340d95f455e3b0373d176a129fb18872ce62c72
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-04-20 18:21:32 +0200

    do not save generated images too frequently

--- autoencoder.py
+++ autoencoder.py
@@ -41,7 +41,7 @@ sampler = samplers.sampler_factory(args, x_train)
 import callbacks
 cbs = [callbacks.FlushCallback()]
 cbs.append(callbacks.get_lr_scheduler(args.nb_epoch, args.lr))
-cbs.append(callbacks.SaveGeneratedCallback(generator, sampler, args.prefix, args.batch_size, args.frequency, args.latent_dim))
+cbs.append(callbacks.SaveGeneratedCallback(generator, sampler, args.prefix, args.batch_size, 20, args.latent_dim))
 cbs.append(callbacks.ImageDisplayCallback(
     x_train, x_test, args,
     ae, encoder, encoder_var, generator, sampler))

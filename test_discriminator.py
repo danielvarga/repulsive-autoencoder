5a37b2be411428853ea574a8be4591fa1a2976a8
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-08-07 06:20:30 -0400

    compare original grad penalty (pushing grads towards +-1) and the correct one (only apply it outside of the (-1,1) range)

--- test_discriminator.py
+++ test_discriminator.py
@@ -19,15 +19,22 @@ disc_size = "small"
 wd = 0.0
 use_bn = False
 batch_size = 200
-dense_dims = [100,100]
-activation = "leakyrelu"
-clipValue = 0.07
+dense_dims = [100,100,100,100,100,100]
+activation = "relu"
+clipValue = 0.01
 verbose=1
-nb_epoch = 3
+nb_epoch = 1
 prefix = "pictures/testdisc"
 
 
-x_train = 10.0 * np.sign(np.random.randint(0,3, size=trainSize) - 1.5) # -10 with 2/3 probability and 10 with 1/3 probability
+# x_train = 10.0 * np.sign(np.random.randint(0,3, size=trainSize) - 1.5) # -10 with 2/3 probability and 10 with 1/3 probability
+
+x_train = np.random.randint(0,5, size=trainSize)
+x_train[x_train < 1] = -10.0
+x_train[x_train == 1] = 0.0
+x_train[x_train == 2] = 0.0
+x_train[x_train > 2] = 10.0
+
 y_train = np.ones(shape=x_train.shape)
 x_generated = np.random.uniform(-10, 10, size=trainSize) # uniform between -10 and 10
 y_generated = - np.ones(shape=x_generated.shape)
@@ -50,26 +57,55 @@ discriminator.summary()
 # y_true = 1 (real_image) or -1 (generated_image)
 # we push the real examples up, the false examples down
 def D_loss(y_true, y_pred):
-    return - K.mean(y_true * y_pred)
+    loss = - K.mean(y_true * y_pred)
+    return loss
 
-clipper = callbacks.ClipperCallback(disc_layers, clipValue)
+def grad_loss(y_true, y_pred):
+    grads = K.gradients(y_pred, disc_input)
+    k1 = K.constant(1.0)
+    grad_penalty = K.maximum(k1, K.abs(grads)) - k1
+    return grad_penalty
+
+def gp_loss(y_true, y_pred):
+    grads = K.gradients(y_pred, disc_input)
+    k1 = K.constant(1.0)
+    grad_penalty = K.square(K.abs(grads) - k1)
+    return grad_penalty
+
+def total_gp(y_true, y_pred):
+    return D_loss(y_true, y_pred) + 10 * gp_loss(y_true, y_pred)
+def total_grad(y_true, y_pred):
+    return D_loss(y_true, y_pred) + 10 * grad_loss(y_true, y_pred)
+
+#clipper = callbacks.ClipperCallback(disc_layers, clipValue)
 optimizer = Adam()
-discriminator.compile(optimizer=optimizer, loss=D_loss)
 
+discriminator.compile(optimizer=optimizer, loss=total_gp, metrics=[D_loss, grad_loss, gp_loss] )
 discriminator.fit(xs, ys,
                   verbose=verbose,
                   shuffle=True,
                   epochs = nb_epoch,
-                  batch_size=batch_size,
-                  callbacks=[clipper]
+                  batch_size=batch_size
                   )
+test_gp = np.linspace(-10, 10, 10 * batch_size)
+test_result_gp = discriminator.predict(test_gp, batch_size=batch_size)
 
-test = np.linspace(-10, 10, 10 * batch_size)
-test_result = discriminator.predict(test, batch_size=batch_size)
+discriminator.compile(optimizer=optimizer, loss=total_grad, metrics=[D_loss, grad_loss, gp_loss] )
+discriminator.fit(xs, ys,
+                  verbose=verbose,
+                  shuffle=True,
+                  epochs = nb_epoch,
+                  batch_size=batch_size
+                  )
+test_grad = np.linspace(-10, 10, 10 * batch_size)
+test_result_grad = discriminator.predict(test_grad, batch_size=batch_size)
 
-name = prefix + "-output-{}.png".format(clipValue)
+name = prefix + "-output.png"
 print "Saving {}".format(name)
-plt.figure()
-plt.scatter(test, test_result)
+#plt.figure()
+f, axes = plt.subplots(2, 1, sharex=True)
+axes[0].scatter(test_gp, test_result_gp)
+axes[1].scatter(test_grad, test_result_grad)
+#plt.scatter(test, test_result)
 plt.savefig(name)
 plt.close()

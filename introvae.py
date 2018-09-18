479f3726ce06f8ac273de71310ebb40bdc7321cd
bbeatrix <bbea.smile@gmail.com>
2018-09-18 22:28:58 +0200

    Developement of introvae

--- introvae.py
+++ introvae.py
@@ -13,7 +13,9 @@ import load_models
 import callbacks
 import samplers
 
-import model_introvae
+import model_resnet
+from model import add_sampling
+import tensorflow as tf
 
 args = params.getArgs()
 print(args)
@@ -44,84 +46,119 @@ data_object = data.load(args.dataset, shape=args.shape, color=args.color)
 x_true_flow = data_object.get_train_flow(args.batch_size)
 args.original_shape = x_train.shape[1:]
 
-print('Define loss functions')
-
-
-def encoder_loss(x, xr):
-    kl_loss_z = 0.5 * K.sum(1 + z_logvar - K.square(z_mean) - K.exp(z_logvar),
-                            axis=-1)
-    kl_loss_zr = 0.5 * K.sum(1 + zr_logvar_ng - K.square(zr_mean_ng) - K.exp(zr_logvar_ng),
-                             axis=-1)
-    kl_loss_zp = 0.5 * K.sum(1 + zp_logvar_ng - K.square(zp_mean_ng) - K.exp(zp_logvar),
-                             axis=-1)
-    recon_loss = 0.5 * K.sum(mean_squared_error(x, xr),
-                             axis=-1)
-    loss = kl_loss_z + args.alpha * K.max(0, args.m - kl_loss_zr) + args.alpha * K.max(0, args.m - kl_loss_zp) + args.beta * recon_loss
-    return loss
-
-
-def decoder_loss(x, xr):
-    kl_loss_zr = 0.5 * K.sum(1 + zr_logvar - K.square(zr_mean) - K.exp(zr_logvar),
-                             axis=-1)
-    kl_loss_zp = 0.5 * K.sum(1 + zp_logvar - K.square(zp_mean) - K.exp(zp_logvar),
-                             axis=-1)
-    recon_loss = 0.5 * K.sum(mean_squared_error(x, xr),
-                             axis=-1)
-    loss = args.alpha * kl_loss_zr + args.alpha * kl_loss_zp + args.beta * recon_loss
-    return loss
-
 
 print('Build networks')
 
-encoder_channels = model_introvae.default_channels('encoder', args.shape)
-decoder_channels = model_introvae.default_channels('decoder', args.shape)
+encoder_channels = model_resnet.default_channels('encoder', args.shape)
+decoder_channels = model_resnet.default_channels('decoder', args.shape)
 
-
-
-print('decoder channels: ', decoder_channels)
+if args.shape == (64, 64):
+    encoder_layers = model_resnet.encoder_layers_introvae_64(encoder_channels, args.encoder_use_bn)
+    decoder_layers = model_resnet.decoder_layers_introvae_64(decoder_channels, args.decoder_use_bn)
+elif args.shape == (1024, 1024):
+    encoder_layers = model_resnet.encoder_layers_introvae_1024(encoder_channels, args.encoder_use_bn)
+    decoder_layers = model_resnet.decoder_layers_introvae_1024(decoder_channels, args.decoder_use_bn)
+else:
+    assert False, 'unknown input size ' + args.shape
 
 encoder_input = Input(batch_shape=[args.batch_size] + list(args.original_shape), name='encoder_input')
 decoder_input = Input(batch_shape=(args.batch_size, args.latent_dim), name='decoder_input')
 
+encoder_output = encoder_input
+for layer in encoder_layers:
+    encoder_output = layer(encoder_output)
+
+decoder_output = decoder_input
+for layer in decoder_layers:
+    decoder_output = layer(decoder_output)
+
+z, z_mean, z_log_var = add_sampling(encoder_output, args.sampling, args.sampling_std, args.batch_size, args.latent_dim, args.encoder_wd)
+
+encoder = Model(inputs=encoder_input, outputs=[z_mean, z_log_var])
+decoder = Model(inputs=decoder_input, outputs=decoder_output)
+
+xr = decoder(z)
+zr_mean, zr_log_var = encoder(xr)
+zr_mean_ng, zr_log_var_ng = encoder(tf.stop_gradient(xr))
+
+zp = K.random_normal(shape=(args.batch_size, args.latent_dim), mean=0.)
+
+xp = decoder(zp)
+zp_mean, zp_log_var = encoder(xp)
+zp_mean_ng, zp_log_var_ng = encoder(tf.stop_gradient(xp))
+
 print('Define optimizer')
 
 if args.optimizer == 'adam':
-    encoder_optimizer = Adam(lr=args.lr)
-    decoder_optimizer = Adam(lr=args.lr)
+    encoder_optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
+    decoder_optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
 elif args.optimizer == 'rmsprop':
-    encoder_optimizer = RMSprop(lr=args.lr)
-    decoder_optimizer = RMSprop(lr=args.lr)
+    encoder_optimizer = tf.train.RMSpropOptimizer(learning_rate=args.lr)
+    decoder_optimizer = tf.train.RMSpropOptimizer(learning_rate=args.lr)
 elif args.optimizer == 'sgd':
-    encoder_optimizer = SGD(lr=args.lr)
-    decoder_optimizer = SGD(lr=args.lr)
+    encoder_optimizer = tf.train.GradientDescentOptimizer(learning_rate=args.lr)
+    decoder_optimizer = tf.train.GradientDescentOptimizer(learning_rate=args.lr)
 
-sampler = samplers.gaussian_sampler
+print('Define loss functions')
 
-def build_networks(encoder_input, decoder_input):
+def l_reg(mean, log_var):
+    return  K.mean(0.5 * K.sum(-1 + log_var - K.square(mean) - K.exp(log_var), axis=-1))
 
-    encoder_output = model_introvae.encoder_layers_introvae(encoder_input,
-                                                            encoder_channels,
-                                                            args.encoder_use_bn)
-    decoder_output = model_introvae.decoder_layers_introvae(decoder_input,
-                                                            decoder_channels,
-                                                            args.decoder_use_bn)
-
-    encoder = Model(inputs=encoder_input, outputs=encoder_output)
-    decoder = Model(inputs=decoder_input, outputs=decoder_output)
-
-    print('Compile models.')
-    encoder.compile(optimizer=encoder_optimizer, loss=encoder_loss)
-    decoder.compile(optimizer=decoder_optimizer, loss=decoder_loss)
-
-    return (encoder, decoder)
-
-(encoder, decoder) = build_networks(encoder_input, decoder_input)
+def l_ae(x, x_decoded):
+    original_dim = np.float32(np.prod(args.original_shape))
+    return K.mean(original_dim * K.sum(mean_squared_error(x, x_decoded), axis=-1))
 
 
+l_reg_z = l_reg(z_mean, z_log_var)
+l_reg_zr_ng = l_reg(zr_mean_ng, zr_log_var_ng)
+l_reg_zp_ng = l_reg(zp_mean_ng, zp_log_var_ng)
+l_reconstruction = l_ae(encoder_input, xr)
+encoder_loss = l_reg_z + args.alpha * K.maximum(0., args.m - l_reg_zr_ng) + args.alpha * K.maximum(0., args.m - l_reg_zp_ng) + args.beta * l_reconstruction
+
+l_reg_zr = l_reg(zr_mean, zr_log_var)
+l_reg_zp = l_reg(zp_mean, zp_log_var)
+decoder_loss = args.alpha * l_reg_zr + args.alpha * l_reg_zp + args.beta * l_reconstruction
 
 print('Encoder')
 encoder.summary()
 print('Generator')
 decoder.summary()
 
+print('Start training')
+
+iterations = x_train.shape[0] // args.batch_size
+
+encoder_params = encoder.trainable_weights
+decoder_params = decoder.trainable_weights
+
+print('Define train step operations')
+encoder_train_op = encoder_optimizer.minimize(encoder_loss, var_list=encoder_params)
+decoder_train_op = decoder_optimizer.minimize(decoder_loss, var_list=decoder_params)
+
+print('Start session')
+with tf.Session() as session:
+    init = tf.global_variables_initializer()
+    session.run(init)
+    # summary_writer = tf.summary.FileWriter("./tmp/tf_logdir/", graph=tf.get_default_graph())
+    for epoch in range(args.nb_epoch):
+        for iteration in range(iterations):
+            print('Epoch: {}/{}, iteration: {}/{}'.format(epoch, args.nb_epoch, iteration, iterations))
+
+            x_true = x_true_flow.next()[0]
+            # z_true = session.run(z, feed_dict={encoder_input: x_true})
+            # z_p = np.random.normal(size=(args.batch_size, args.latent_dim))
+            # x_r = session.run(decoder_output, feed_dict={decoder_input: z_true})
+            # x_p = session.run(decoder_output, feed_dict={decoder_input: z_p})
+
+            _, enc_loss, ae_l, z_lreg, zr_ng_lreg, zp_ng_lreg = session.run([encoder_train_op, encoder_loss, l_reconstruction, l_reg_z, l_reg_zr_ng, l_reg_zp_ng],
+                                                                            feed_dict={encoder_input: x_true})
+
+            print(' Enc_loss: {}, l_ae:{},  l_reg_z: {}, l_reg_zr_ng: {}, l_reg_zp_ng: {}'.format(enc_loss, ae_l, z_lreg, zr_ng_lreg, zp_ng_lreg))
+
+            _, dec_loss, ae_l, zr_lreg, zp_lreg = session.run([decoder_train_op, decoder_loss, l_reconstruction, l_reg_zr, l_reg_zp],
+                                                              feed_dict={encoder_input: x_true})
+
+            print(' Dec_loss: {}, l_ae:{}, l_reg_zr: {}, l_reg_zp: {}'.format(dec_loss, ae_l, zr_lreg, zp_lreg))
+
+            # print('Epoch: {}/{}, iteration: {}/{}, enc_loss: {}, dec_loss: {}'.format(epoch, args.nb_epoch, iteration, iterations, enc_loss, dec_loss))
 print('OK')
